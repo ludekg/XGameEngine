561f2c5ea38eac41e4b0fb5fdf4de1727627f92d
ludekg <grusludek@gmail.com>
2014-11-09 23:08:09 +0100

    main

--- src/com/xgame/engine/XEngine.java
+++ src/com/xgame/engine/XEngine.java
@@ -1,6 +1,9 @@
 package com.xgame.engine;
 
-public class XEngine implements  Runnable {
+import javax.swing.*;
+import java.awt.*;
+
+public class XEngine extends Canvas implements Runnable {
 
     //Constants
     public static final int WIDTH =  300;
@@ -8,7 +11,15 @@ public class XEngine implements  Runnable {
     public static final int SCALE = 3;
 
     private Thread thread;
+    private JFrame mainFrame;
     private boolean running;
+
+    //Constructor
+    public XEngine(){
+        Dimension frameSize = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+        setPreferredSize(frameSize);
+        this.mainFrame = new JFrame();
+    }
 
     /**
      * Starts the game thread.
@@ -33,7 +44,21 @@ public class XEngine implements  Runnable {
 
     @Override public void run() {
         while(running){
-
+            System.out.println("Game is running..");
         }
+    }
+
+    public static void main(String[] args){
+        XEngine engine = new XEngine();
+        //Graphics settings
+        engine.mainFrame.setResizable(false);
+        engine.mainFrame.setTitle("XGame");
+        engine.mainFrame.add(engine);
+        engine.mainFrame.pack();
+        engine.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        engine.mainFrame.setLocationRelativeTo(null);
+        engine.mainFrame.setVisible(true);
+
+        engine.startGame();
     }
 }
